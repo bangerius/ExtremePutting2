668340e772ccf22e67ba24f6b466747e21746893
Sebastian Bangerius <sebastian.bangerius@gozinto.se>
2014-05-20 20:39:01 +0200

    More holes

--- ExtremePutting2/src/Hole.java
+++ ExtremePutting2/src/Hole.java
@@ -1,12 +1,37 @@
 import java.awt.Image;
 
 
-public class Hole extends Renderable{
-
+public class Hole extends Renderable implements ColisionMate{
+	Shape shape; //This is for handling colisions. I don't know if there will be support for other than round and square.
 	public Hole(Image image, double x, double y) {
 		super(image, x, y);
-		// TODO Auto-generated constructor stub
+		shape = new Circle(image.getWidth(null)/5);
+	}
+
+	@Override
+	public MyVector getPosition() {
+		return new MyVector(xpos, ypos);
+	}
+
+	@Override
+	public Shape getShape() {
+		return null;
+	}
+
+	@Override
+	public MyVector getSpeed() {
+		// TODO Auto-generated method stub
+		return new MyVector(0,0);
+	}
+
+	@Override
+	public void setSpeed(MyVector v) {
 		
+	}
+
+	@Override
+	public double getMass() {
+		return 700000;
 	}
 
 }
