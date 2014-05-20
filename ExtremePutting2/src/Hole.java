7ca2d18c968f6fbe55d3e7bdfa76acc79223712d
Sebastian Bangerius <sebastian.bangerius@gozinto.se>
2014-05-20 20:32:22 +0200

    Added Hole

---
+++ ExtremePutting2/src/Hole.java
@@ -0,0 +1,12 @@
+import java.awt.Image;
+
+
+public class Hole extends Renderable{
+
+	public Hole(Image image, double x, double y) {
+		super(image, x, y);
+		// TODO Auto-generated constructor stub
+		
+	}
+
+}
